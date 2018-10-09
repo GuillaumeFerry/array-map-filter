6a3745635ea0c37ae30e60b81f1094f9a266024e
GFerry <ferry.g@neuf.fr>
2018-10-09 18:36:52 +0200

    FILTER - ex6

--- filter/ex6.js
+++ filter/ex6.js
@@ -26,8 +26,25 @@ Sortie attendue:
 
 */
 
-function filterOffensiveComments(comments, bannedWords) {
+const filterOffensiveComments = (comments, bannedWords) => {
+    const result = comments.filter((word) => {
+        for (let i = 0; i < bannedWords.length; i++ ) {
+            if (word.toLowerCase().includes(bannedWords[i])) 
+            return false
+            }
+        return true
+        }
+    )
+    return result
 }
+
+/* console.log(filterOffensiveComments([
+    "Very useful tutorial, thank you so much!",
+    "React is not a damn framework, it's a LIBRARY",
+    "Why you put bloody kitten pictures in a tech tutorial is beyond me!",
+    "Which one is better, React or Angular?",
+    'There is no "better", it depends on your use case, DAMN YOU'
+  ], ['bloody', 'damn'])); */
 
 // Ne pas modifier l'export
 module.exports = filterOffensiveComments;
